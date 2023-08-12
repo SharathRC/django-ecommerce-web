0fbc059ddb026c9501fc2de83626111d672aa995
Sharath RC <sharathr.sh@gmail.com>
2023-08-12 15:57:42 +0200

    add 'core' app

---
+++ djecommerce/core/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class OrderItem(models.Model):
+    pass
+
+
+class Order(models.Model):
+    pass
+
--- djecommerce/djecommerce/settings.py
+++ djecommerce/djecommerce/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'django.contrib.sites',
+
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +127,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#Allauth
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
+
+SITE_ID = 1
