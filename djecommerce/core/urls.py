09333c4fed10ad4c791729bd912cfd991e48df2d
Sharath RC <sharathr.sh@gmail.com>
2023-08-13 23:55:50 +0200

    add login and register pages

--- djecommerce/core/urls.py
+++ djecommerce/core/urls.py
@@ -6,10 +6,18 @@ from django.contrib.auth.views import (
     PasswordChangeDoneView
 )
 
-from .views import item_list
+from .views import (
+    item_list,
+    register,   
+)
 
 
 urlpatterns = [
     path('', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('password-change/', PasswordChangeView.as_view(), name='password_change'),
+    path('password-change-done/', PasswordChangeDoneView.as_view(), name='password_change_done'),
+    path('register/', register, name='register'),
+    
     path('items/', item_list, name='item_list'),
 ]
