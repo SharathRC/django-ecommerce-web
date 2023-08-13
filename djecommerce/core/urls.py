13e7096e479b759103ec621eed631995e2973933
Sharath RC <sharathr.sh@gmail.com>
2023-08-13 23:35:49 +0200

    add login to core url

--- djecommerce/core/urls.py
+++ djecommerce/core/urls.py
@@ -1,10 +1,15 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from django.contrib.auth.views import (
+    LoginView, 
+    LogoutView, 
+    PasswordChangeView, 
+    PasswordChangeDoneView
+)
 
 from .views import item_list
 
-app_name = 'core'
 
 urlpatterns = [
+    path('', LoginView.as_view(), name='login'),
     path('items/', item_list, name='item_list'),
 ]
