987302351de1e0728075526986ef2ff54c23cc0c
Sharath RC <sharathr.sh@gmail.com>
2023-08-13 23:36:19 +0200

    add core url to main url

--- djecommerce/djecommerce/urls.py
+++ djecommerce/djecommerce/urls.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
-from django.urls import path
-from django.conf.urls import include
-
-
+from django.urls import path, include
 
 urlpatterns = [
-    path('', include('core.urls', namespace='core')),
+    path('admin/', admin.site.urls),
+    path('', include('core.urls'))
 ]
