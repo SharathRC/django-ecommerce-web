5f32a5a19d61d85c75e6e9f6ee9ecf118c082dde
Sharath RC <sharathr.sh@gmail.com>
2023-08-19 11:54:42 +0200

    add store app

--- djecommerce/core/urls.py
+++ djecommerce/core/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from .views import homepage
+from .views import homepage, about
 
 urlpatterns = [
     path('', homepage, name='homepage'),
+    path('about/', about, name='about'),
 ]
