3c72a3f01570cc4970de4b2f1052492735ff4df5
Sharath RC <sharathr.sh@gmail.com>
2023-08-19 12:15:33 +0200

    add products

--- djecommerce/core/views.py
+++ djecommerce/core/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
+from store.models import Product
 
 
 def homepage(request):
-    return render(request, 'homepage.html')
+    products = Product.objects.all()[0:6]
+    context = {'products': products}
+    
+    return render(request, 'homepage.html', context)
 
 def about(request):
     return render(request, 'about.html')
--- djecommerce/store/models.py
+++ djecommerce/store/models.py
@@ -25,8 +25,14 @@ class Product(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     
+    class Meta:
+        ordering = ('-created_at',)
+    
     def __str__(self) -> str:
         return self.title
+
+    def get_display_price(self) -> float:
+        return self.price/100
     
         
     
