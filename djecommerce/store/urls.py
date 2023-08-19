ff30463b04451863a27dbb999cd783feded567cd
Sharath RC <sharathr.sh@gmail.com>
2023-08-19 13:19:21 +0200

    add menu

--- djecommerce/store/urls.py
+++ djecommerce/store/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from .views import product_detail
+from .views import product_detail, category_detail
 
 urlpatterns = [
-    path('<slug:slug>/', product_detail, name='product_detail'),
+    path('<slug:category_slug>/<slug:slug>/', product_detail, name='product_detail'),
+    path('<slug:slug>/', category_detail, name='category_detail'),
 ]
