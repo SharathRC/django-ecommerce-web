9736a1dbbbce5db18900dc55bd85ec67e8a620b4
Sharath RC <sharathr.sh@gmail.com>
2023-08-19 09:53:56 +0200

    updated urls

--- djecommerce/core/urls.py
+++ djecommerce/core/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from .views import homepage
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('core.urls'))
+    path('', homepage, name='homepage'),
 ]
