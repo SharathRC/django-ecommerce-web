8d2dad82534c201de0bec01bdc92656b9eacd089
Sharath RC <sharathr.sh@gmail.com>
2023-08-22 23:53:44 +0200

    add search button

--- djecommerce/store/urls.py
+++ djecommerce/store/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from .views import product_detail, category_detail
+from .views import product_detail, category_detail, search
 
 urlpatterns = [
+    path('search/', search, name='search'),
     path('<slug:category_slug>/<slug:slug>/', product_detail, name='product_detail'),
     path('<slug:slug>/', category_detail, name='category_detail'),
 ]
