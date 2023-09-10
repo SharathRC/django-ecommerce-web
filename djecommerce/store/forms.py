cd4ad440cbfd9ea5e779eea2927181af09051e78
Sharath RC <sharathr.sh@gmail.com>
2023-09-10 22:41:22 +0200

    update styles

--- djecommerce/store/forms.py
+++ djecommerce/store/forms.py
@@ -13,3 +13,31 @@ class ProductForm(forms.ModelForm):
             "price",
             "image",
         )
+        widgets = {
+            "category": forms.Select(
+                attrs={
+                    "class": "w-full px-3 py-2 my-2 border border-gray-200",
+                }
+            ),
+            "title": forms.TextInput(
+                attrs={
+                    "class": "w-full px-3 py-2 my-2 border border-gray-200",
+                }
+            ),
+            "description": forms.Textarea(
+                attrs={
+                    "class": "w-full px-3 py-2 my-2 border border-gray-200",
+                    "rows": 3,
+                }
+            ),
+            "price": forms.TextInput(
+                attrs={
+                    "class": "w-full px-3 py-2 my-2 border border-gray-200",
+                }
+            ),
+            "image": forms.FileInput(
+                attrs={
+                    "class": "w-full px-3 py-2 my-2 border border-gray-200",
+                }
+            ),
+        }
