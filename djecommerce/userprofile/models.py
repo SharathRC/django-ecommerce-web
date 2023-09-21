1ccdeb220fed6c97242802e4e16669ef783d695a
Sharath RC <sharathr.sh@gmail.com>
2023-09-21 18:26:31 +0200

    fianlize cart feature

--- djecommerce/userprofile/models.py
+++ djecommerce/userprofile/models.py
@@ -6,6 +6,7 @@ class UserProfile(models.Model):
     user = models.OneToOneField(
         User, related_name="userprofile", on_delete=models.CASCADE
     )
+    is_vendor = models.BooleanField(default=False)
 
     def __str__(self) -> str:
         return self.user.username
