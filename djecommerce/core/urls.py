db95f9f3a64a9f9c18079be4af0e8179941b4143
Sharath RC <sharathr.sh@gmail.com>
2023-10-15 15:31:58 +0200

    add robots txt

--- djecommerce/core/urls.py
+++ djecommerce/core/urls.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import TemplateView
 
 from .views import homepage, about
 
 urlpatterns = [
     path("", homepage, name="homepage"),
+    path(
+        "robots.txt",
+        TemplateView.as_view(template_name="robots.txt", content_type="text/plain"),
+    ),
     path("about/", about, name="about"),
 ]
